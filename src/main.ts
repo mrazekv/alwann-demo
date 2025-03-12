cda7c7792a2076198de09a8ce84d8e9aba4f96b0
zvasicek <37452722+zvasicek@users.noreply.github.com>
2025-03-12 17:50:57 +0100

    cleaning

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,4 @@ import App from './App.vue'
 // Import our custom CSS
 import './style.scss'
 
-// Import all of Bootstrap's JS
-//import * as bootstrap from 'bootstrap'
-
 createApp(App).mount('#app') 
