a04e293c1b76585692c8a524213a060430a66627
zvasicek <37452722+zvasicek@users.noreply.github.com>
2025-03-16 22:22:02 +0100

    alg2

--- src/components/Algorithm.vue
+++ src/components/Algorithm.vue
@@ -1,35 +1,40 @@
 <template>
+  <div>
+    <h3 class="fs-4">HW Accelerator of Neural Networks</h3>
     <div class="algorithm">
-        <img src="/resnet_alg2.svg" alt="ResNet Algorithm" />
+    <img src="/resnet_alg2.svg" alt="ResNet Algorithm" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 import { defineProps, defineComponent } from 'vue';
 const props = defineProps({
-    /*scale: {
-      type: Number,
-      default: 100,
-    },*/
-  });
+  /*scale: {
+    type: Number,
+    default: 100,
+  },*/
+});
 </script>
 
 <style scoped>
-/* Add any styles you need here */
-.algorithm {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 20px;
-    max-width: 100%;    
-
-    
+h3 {
+  text-align: center;
 }
 
-.algorithm > img {
-      max-width: 100%;
-      height: auto;
-      overflow-x: auto;
+/* Add any styles you need here */
+.algorithm {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  max-width: 100%;
+}
+
+.algorithm>img {
+  max-width: 100%;
+  height: auto;
+  overflow-x: auto;
 }
 </style>
